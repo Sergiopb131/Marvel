96f980d820a7c27195baa21d471869ebf5cf7866
Sergiopb131 <sergio.peraltab@campusucc.edu.co>
2023-05-15 10:08:44 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-          <h1>MARVEL</h1>
+          <h1>PERSONAJES</h1>
           <div className="row row-cols-1 row-cols-md-3 g-4">
 
             { personajes.map( per=>(
