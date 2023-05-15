8737ebe9ce64c18ecec086bdef1d0efe90215693
Sergiopb131 <sergio.peraltab@campusucc.edu.co>
2023-05-15 17:59:47 -0500

    14 commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import {useEffect, useState } from 'react';
-
-
-// https://gateway.marvel.com:443/v1/public/characters?apikey=6e578bd8669d3b4d78b0db5e5b44e1f6
-
-// Key privada: 28e175412cf48fc08442b9540feefa63f86da148
-// Key Publica: 6e578bd8669d3b4d78b0db5e5b44e1f6
-// ts: 1 
-
-// hashmd5(1llaveprivadallavepublica)
-// 128e175412cf48fc08442b9540feefa63f86da1486e578bd8669d3b4d78b0db5e5b44e1f6
-// hash: ebb925d085a862020285610c16aa3364
+import { useEffect, useState } from 'react';
 
 function App() {
   const [personajes, setPersonajes] = useState([]);
 
   useEffect(() => {
     axios.get('https://ubycwr5j5wgg6ijrc2dg6qermm0wpkwl.lambda-url.us-east-1.on.aws/').then((res) => {
-        setPersonajes(res.data);
-      })
-      .catch((error) => console.log(error));
+      setPersonajes(res.data);
+    }).catch((error) => {
+      console.log(error);
+    });
   }, []);
-
-  console.log(personajes);
 
   return (
     <div className="App">
@@ -32,13 +20,8 @@ function App() {
       <div className="row row-cols-1 row-cols-md-3 g-4">
         {personajes.map((per) => (
           <div className="col" key={per.id}>
-            <div className="card" style={{ width: '18rem', height: '18rem' }}>
-              <img
-                src={`${per.thumbnail}`}
-                className="card-img-top"
-                style={{ width: '80%', height: '80%' }}
-                alt="Character Thumbnail"
-              />
+            <div className="card" style={{ width: "18rem", height: "18rem" }}>
+              <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} className="card-img-top" style={{ width: "80%", height: "80%" }} />
               <div className="card-body">
                 <p className="card-text">{per.name}</p>
               </div>
