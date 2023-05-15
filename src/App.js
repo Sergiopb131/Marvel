83375cc33ca91a1f57d28442a023ffe4313ec66d
Sergiopb131 <sergio.peraltab@campusucc.edu.co>
2023-05-15 16:45:59 -0500

    12 commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 // https://gateway.marvel.com:443/v1/public/characters?apikey=6e578bd8669d3b4d78b0db5e5b44e1f6
@@ -14,37 +13,44 @@ import {useEffect, useState } from 'react';
 // 128e175412cf48fc08442b9540feefa63f86da1486e578bd8669d3b4d78b0db5e5b44e1f6
 // hash: ebb925d085a862020285610c16aa3364
 
+
 function App() {
+  const [personajes, setPersonajes] = useState([]);
 
-  const [personajes, setPersonajes]=useState([])
+  async function fetchData() {
+    try {
+      const response = await fetch('/.netlify/functions/lambda');
+      const data = await response.json();
+      setPersonajes(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  useEffect(()=>{
-    axios.get('https://vxtce5sgoqfplkabwjjzlsewqi0amxvc.lambda-url.us-east-1.on.aws/').then(res=>{
-      setPersonajes(res.data.data.results)
-     
-    }).catch(error=>console.log(error))
-  },[])
-
-  console.log(personajes)
-
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-          <h1>PERSONAJES</h1>
-          <div className="row row-cols-1 row-cols-md-3 g-4">
-
-            { personajes.map( per=>(
-                 <div className="col" key={per.id}>
-                 <div className="card" style={{width: "18rem", height:"18rem"}}>
-                     <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} className="card-img-top" style={{width: "80%", height:"80%"}} />
-                     <div className="card-body">
-                       <p className="card-text">{per.name}</p>
-                     </div>
-                 </div>
-             </div>
-            ))
-            } 
+      <h1>PERSONAJES</h1>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {personajes.map((per) => (
+          <div className="col" key={per.id}>
+            <div className="card" style={{ width: '18rem', height: '18rem' }}>
+              <img
+                src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+                alt={per.name}
+                className="card-img-top"
+                style={{ width: '80%', height: '80%' }}
+              />
+              <div className="card-body">
+                <p className="card-text">{per.name}</p>
+              </div>
+            </div>
           </div>
+        ))}
+      </div>
     </div>
   );
 }
