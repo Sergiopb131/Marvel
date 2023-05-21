4111b4710d27959c04f82a9697474e1e5acafe2c
Sergiopb131 <sergio.peraltab@campusucc.edu.co>
2023-05-20 21:15:28 -0500

    22 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react';
+//import logo from './logo.svg';
+import './App.css';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [personajes, setPersonajes] = useState([]);
 
   useEffect(() => {
-    axios.get('https://y3fuq7lqf4dxwdsz3cxqw3arla0vbqyl.lambda-url.us-east-1.on.aws/').then(res => {
-      setPersonajes(res.data.body);
-    }).catch(error => console.log(error));
+    axios
+      .get('https://y3fuq7lqf4dxwdsz3cxqw3arla0vbqyl.lambda-url.us-east-1.on.aws/')
+      .then((res) => {
+        setPersonajes(res.data.body.data.results);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   console.log(personajes);
@@ -16,10 +21,15 @@ function App() {
     <div className="App">
       <h1>PERSONAJES</h1>
       <div className="row row-cols-1 row-cols-md-3 g-4">
-        {personajes.map(per => (
+        {personajes.map((per) => (
           <div className="col" key={per.id}>
-            <div className="card" style={{ width: "18rem", height: "18rem" }}>
-              <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} className="card-img-top" style={{ width: "80%", height: "80%" }} alt={per.name} />
+            <div className="card" style={{ width: '18rem', height: '18rem' }}>
+              <img
+                src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+                className="card-img-top"
+                style={{ width: '80%', height: '80%' }}
+                alt={per.name}
+              />
               <div className="card-body">
                 <p className="card-text">{per.name}</p>
               </div>
